094a8b4a6fea8549b992907a5f13918f2fc8f3d8
jillianli <jillianli@outlook.com>
2024-07-04 23:13:04 -0700

    some changes of the paragraph

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,9 @@
 export default function Lab1() {
     return (
-    <div id="wd-lab1">
-    <h2>Lab 1</h2>
-    </div>
+      <div id="wd-lab1">
+        <h2>Lab 1</h2>
+        <p>Some Changes for Lab 1</p>
+      </div>
     );
-}
+  }
+  
