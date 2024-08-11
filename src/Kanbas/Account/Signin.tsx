afb8a1eb279038304df80308c74024fd3616e8a3
lihejia09 <jillianli@outlook.com>
2024-08-10 23:48:28 -0700

    fix bug

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -9,6 +9,7 @@ export default function Signin() {
   const [credentials, setCredentials] = useState<any>({});
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const signin = async () => {
     try {
       const currentUser = await client.signin(credentials);
@@ -23,11 +24,23 @@ export default function Signin() {
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
       {error && <div className="wd-error alert alert-danger">{error}</div>}
-      
-      <input id="wd-username" onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-        value={credentials.username} className="form-control mb-2" placeholder="username" />
-      <input id="wd-password" onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-        value={credentials.password} className="form-control mb-2" placeholder="password" type="password" />
+
+      <input
+        id="wd-username"
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        value={credentials.username}
+        className="form-control mb-2"
+        placeholder="username"
+      />
+
+      <input
+        id="wd-password"
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        value={credentials.password}
+        className="form-control mb-2"
+        placeholder="password"
+        type="password"
+      />
       <button id="wd-signin-btn" onClick={signin} className="btn btn-primary w-100 mb-2"> Sign in </button>
       <br />
       <Link id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
